018a7e63f5b281b026c67fc77005ca19b2b4f405
Conor Smith <conorjbsmith@gmail.com>
2019-09-28 20:46:00 +0100

    Extract presentation logic from controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,9 @@
 namespace ConorSmith\Music\Http\Controllers;
 
 use Carbon\Carbon;
+use ConorSmith\Music\Infrastructure\TemplateVariables\Album as AlbumTemplateVariable;
 use ConorSmith\Music\Model\Album;
 use ConorSmith\Music\Model\AlbumRepository;
-use ConorSmith\Music\Model\Artist;
 use ConorSmith\Music\Model\DiscographyRepository;
 
 class HomeController extends Controller
@@ -27,7 +27,11 @@ class HomeController extends Controller
         $albums = $this->albumRepo->findForThisWeek();
 
         return view('home', [
-            'albums' => $this->transformAlbums($albums),
+            'albums' => collect($albums)
+                ->map(function (Album $album) {
+                    return AlbumTemplateVariable::present($album);
+                })
+                ->toArray(),
         ]);
     }
 
@@ -39,7 +43,11 @@ class HomeController extends Controller
             'thisWeek' => Carbon::now("Europe/Dublin")->dayOfWeek === Carbon::MONDAY
                 ? new Carbon("today", "Europe/Dublin")
                 : new Carbon("last Monday", "Europe/Dublin"),
-            'albums' => $this->transformAlbums($albums),
+            'albums' => collect($albums)
+                ->map(function (Album $album) {
+                    return AlbumTemplateVariable::present($album);
+                })
+                ->toArray(),
         ]);
     }
 
@@ -50,70 +58,13 @@ class HomeController extends Controller
         return view('artists', [
             'artists' => collect($discographies)
                 ->map(function ($discography) {
-                    return $this->transformAlbums($discography->getAlbums());
+                    return collect($discography->getAlbums())
+                        ->map(function (Album $album) {
+                            return AlbumTemplateVariable::present($album);
+                        })
+                        ->toArray();
                 })
                 ->toArray(),
         ]);
-    }
-
-    private function transformAlbums(array $albums)
-    {
-        return collect($albums)
-            ->map(function ($album) {
-                return $this->transformAlbum($album);
-            })
-            ->toArray();
-    }
-
-    private function transformAlbum(Album $album)
-    {
-        return [
-            'title'         => strval($album->getTitle()),
-            'artist'        => strval($album->getArtist()->getName()),
-            'listened_at'   => $album->getListenedAt()->getDate()->format('d/m/Y'),
-            'year'          => strval($album->getReleaseDate()->getValue()),
-            'rating'        => $album->getRating()->getValue(),
-            'artist_colour' => $this->getColourForArtist($album->getArtist()),
-        ];
-    }
-
-    private function getColourForArtist(Artist $artist)
-    {
-        $classesByLetter = [
-            'a' => "material-red",
-            'b' => "material-red",
-            'c' => "material-pink",
-            'd' => "material-pink",
-            'e' => "material-purple",
-            'f' => "material-purple",
-            'g' => "material-deeppurple",
-            'h' => "material-deeppurple",
-            'i' => "material-indigo",
-            'j' => "material-indigo",
-            'k' => "material-lightblue",
-            'l' => "material-lightblue",
-            'm' => "material-cyan",
-            'n' => "material-cyan",
-            'o' => "material-teal",
-            'p' => "material-teal",
-            'q' => "success",
-            'r' => "success",
-            's' => "material-lightgreen",
-            't' => "material-lightgreen",
-            'u' => "material-lime",
-            'v' => "material-lime",
-            'w' => "material-lightyellow",
-            'x' => "material-lightyellow",
-            'y' => "material-orange",
-            'z' => "material-orange",
-        ];
-
-        $artistFirstLetter = strtolower(substr($artist->getName(), 0, 1));
-
-        if (!array_key_exists($artistFirstLetter, $classesByLetter)) {
-            return "material-red";
-        }
-
-        return $classesByLetter[$artistFirstLetter];
     }
 }
